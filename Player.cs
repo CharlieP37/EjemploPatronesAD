13c5902e4afbca58a2314ff1176e4ea0d26ef4cf
CharlieP37 <70582977+CharlieP37@users.noreply.github.com>
2024-04-19 22:06:09 -0600

    Player atributes added

--- Player.cs
+++ Player.cs
@@ -9,6 +9,37 @@ namespace EjemploPatronesAD
 {
     public class Player : Prototype<Player>
     {
+        string name;
+        int health;
+        int level;
+        int experience;
+        int score;
+        int strength;
+        int agility;
+        int stamina;
+        int intellect;
+        int spirit;
+
+        public string Name { get => name; set => name = value; }
+
+        int Health { get => health; set => health = value; }
+
+        int Level { get => level; set => level = value; }
+
+        int Experience { get => experience; set => experience = value; }
+
+        int Score { get => score; set => score = value; }
+
+        int Strength { get => strength; set => strength = value; }
+
+        int Agility { get => agility; set => agility = value; }
+
+        int Stamina { get => stamina; set => stamina = value; }
+
+        int Intellect { get => intellect; set => intellect = value; }
+
+        int Spirit { get => spirit; set => spirit = value; }
+
         public Player Fullclone()
         {
             Player clone = (Player)this.MemberwiseClone();
