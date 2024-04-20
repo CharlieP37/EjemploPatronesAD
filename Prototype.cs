2f8f7f0255338c94c7d9ffc7989c1acc078c25c0
CharlieP37 <70582977+CharlieP37@users.noreply.github.com>
2024-04-19 19:40:49 -0600

    Prototype Interface

---
+++ Prototype.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploPatronesAD
+{
+    public interface Prototype <T>
+    {
+        public T Fullclone();
+
+        public T PartialClone();
+    }
+}
