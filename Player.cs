9833bbd159acc346074d5d0d696f2f107726165d
CharlieP37 <70582977+CharlieP37@users.noreply.github.com>
2024-04-19 19:45:48 -0600

    Player Class

---
+++ Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploPatronesAD
+{
+    public class Player : Prototype<Player>
+    {
+        public Player Fullclone()
+        {
+            Player clone = (Player) this.MemberwiseClone();
+            return clone;
+        }
+
+        public Player PartialClone()
+        {
+            return (Player) this.MemberwiseClone();
+        }
+    }
+}
