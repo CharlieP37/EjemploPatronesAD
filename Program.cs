12624b5552b2178125954835d29a207183796700
CharlieP37 <70582977+CharlieP37@users.noreply.github.com>
2024-04-19 20:07:16 -0600

    Console Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using EjemploPatronesAD;
+
+string? Input = "";
+
+Console.WriteLine("Prueba de patrón de diseño PROTOTIPOS");
+Console.WriteLine("Carlos Pop - 1069920\nDiego Gil - \nDiego Calel - \n");
+Console.WriteLine("Personaje default:");
+
+Player DefaultPlayer = new Player();
+
+Console.WriteLine("\n");
+
+while (Input != "3" || Input == null)
+{
+    Console.WriteLine("Ingrese 1 para hacer una copia completa, 2 para una copia parcial o 3 para salir.");
+    Console.Write("Solicitud: ");
+    Input = Console.ReadLine();
+
+    if (Input == "1") 
+    {
+
+    }
+    else if (Input == "2") 
+    {
+        
+    }
+}
